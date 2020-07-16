379bc97ee1787b1c24c33ae2e3b264285a8fff0c
Luciano D'Agata <martinvilla481@gmail.com>
2020-07-16 02:28:14 -0300

    avances TPE_05 usando grid_area reproductor de musica fijo

--- TPE_05/js/main.js
+++ TPE_05/js/main.js
@@ -3,15 +3,26 @@
 document.addEventListener("DOMContentLoaded", e => {
 
     let fondoBlur = document.querySelector(".fondo-blur");
-    let hamburgesa = document.querySelector("#hamburgesa")
+    let hamburgesa = document.querySelector("#hamburgesa");
+    let botones = document.querySelector(".botones");
+
     hamburgesa.addEventListener("click", e => {
-        let botones = document.querySelector(".botones");
+        efectoHamburgesa();
+    });
+
+    fondoBlur.addEventListener("click", e => {
+        efectoHamburgesa();
+    });
+
+    function efectoHamburgesa() {
         botones.classList.toggle("desplegarNavBar");
         hamburgesa.classList.toggle("activeHamburgesa");
         if (botones.classList.contains("desplegarNavBar")) {
             fondoBlur.style.setProperty("opacity", "1");
+            fondoBlur.removeAttribute("hidden");
         } else {
             fondoBlur.style.setProperty("opacity", "0");
+            fondoBlur.setAttribute("hidden", true);
         }
-    });
+    }
 });
