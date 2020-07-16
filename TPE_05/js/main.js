9f1932371a28da2c1c9d5ce66ad875ede0adb098
Luciano D'Agata <martinvilla481@gmail.com>
2020-07-15 22:11:54 -0300

    avances con TPE_05 fondo difuminado

--- TPE_05/js/main.js
+++ TPE_05/js/main.js
@@ -2,10 +2,16 @@
 
 document.addEventListener("DOMContentLoaded", e => {
 
+    let fondoBlur = document.querySelector(".fondo-blur");
     let hamburgesa = document.querySelector("#hamburgesa")
     hamburgesa.addEventListener("click", e => {
         let botones = document.querySelector(".botones");
         botones.classList.toggle("desplegarNavBar");
         hamburgesa.classList.toggle("activeHamburgesa");
+        if (botones.classList.contains("desplegarNavBar")) {
+            fondoBlur.style.setProperty("opacity", "1");
+        } else {
+            fondoBlur.style.setProperty("opacity", "0");
+        }
     });
 });
