f7ae423f1aeee94c80cbbe132e8e501f2c1e1c4b
Luciano D'Agata <martinvilla481@gmail.com>
2020-07-18 04:04:10 -0300

    avances con TPE_05 boton filtro

--- TPE_05/js/main.js
+++ TPE_05/js/main.js
@@ -8,6 +8,8 @@ document.addEventListener("DOMContentLoaded", e => {
     let botonPerfil = document.querySelector(".container_IcoProfile");
     let container_buttons2 = document.querySelector(".container_buttons2");
     let containerDropdown = document.querySelector(".container_dropdown");
+    let containerFiltro = document.querySelector(".container_filtro ");
+    let filtroDespregable = document.querySelector(".container_filtro_despregable ");
 
     hamburguesa.addEventListener("click", e => {
         efectoHamburguesa();
@@ -32,5 +34,17 @@ document.addEventListener("DOMContentLoaded", e => {
     botonPerfil.addEventListener("click", e => {
         container_buttons2.classList.toggle("moveTop");
         containerDropdown.firstElementChild.classList.toggle("rotar");
+    });
+
+    containerFiltro.addEventListener("click", e => {
+        filtroDespregable.classList.toggle("moveTopFiltro");
+        containerFiltro.classList.toggle("activeFiltro");
+        // if (filtroDespregable.classList.contains("moveTopFiltro")) {
+        //     fondoBlur.style.setProperty("opacity", "1");
+        //     fondoBlur.removeAttribute("hidden");
+        // } else {
+        //     fondoBlur.style.setProperty("opacity", "0");
+        //     fondoBlur.setAttribute("hidden", true);
+        // }
     });
 });
